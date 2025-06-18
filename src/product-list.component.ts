4a30e8fe614c7e5436d4761858caeef94066ce82
Antonio Criado <140711500+AntonioCReyes@users.noreply.github.com>
2025-06-18 15:57:57 +0200

    Add Angular Material note

---
+++ src/product-list.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { ProductService } from './product.service';
+
+@Component({
+  selector: 'product-list',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  template: `
+    <h2>Products</h2>
+    <ul>
+      @for (product of products(); track product.id) {
+        <li>
+          <a [routerLink]="['/product', product.id]">{{ product.name }}</a>
+        </li>
+      }
+    </ul>
+  `,
+})
+export class ProductListComponent {
+  products = this.service.products;
+
+  constructor(private service: ProductService) {}
+}
