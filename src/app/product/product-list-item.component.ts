743319356cd115b1c3173a207ab4f8f4ff8bb9a6
Antonio Criado <140711500+AntonioCReyes@users.noreply.github.com>
2025-06-19 21:54:10 +0200

    use material card for product list items

--- src/app/product/product-list-item.component.ts
+++ src/app/product/product-list-item.component.ts
@@ -1,13 +1,33 @@
 import { Component, ChangeDetectionStrategy, input } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import {
+  MatCard,
+  MatCardContent,
+  MatCardImage,
+  MatCardTitle,
+  MatCardFooter,
+  MatCardActions,
+} from '@angular/material/card';
+import { MatButton } from '@angular/material/button';
 import { Product } from './product';
-import {ProductSkeletonComponent} from './product-skeleton.component';
+import { ProductSkeletonComponent } from './product-skeleton.component';
 
 @Component({
   selector: 'product-list-item',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterLink, NgOptimizedImage, ProductSkeletonComponent],
+  imports: [
+    RouterLink,
+    NgOptimizedImage,
+    MatCard,
+    MatCardContent,
+    MatCardImage,
+    MatCardTitle,
+    MatCardFooter,
+    MatCardActions,
+    MatButton,
+    ProductSkeletonComponent,
+  ],
   styles: [
     `
       .item {
@@ -22,17 +42,26 @@ import {ProductSkeletonComponent} from './product-skeleton.component';
   ],
   template: `
     @if (product(); as pr) {
-      <div class="item">
+      <mat-card class="item">
         <a [routerLink]="['/products', pr.id]">
           <img
+            mat-card-image
             [ngSrc]="pr.imageUrl"
             alt="{{ pr.name }}"
             width="100"
             height="100"
           />
         </a>
-        <span>{{ pr.name }}</span>
-      </div>
+        <mat-card-content>
+          <h3 mat-card-title>{{ pr.name }}</h3>
+        </mat-card-content>
+        <mat-card-footer>
+          <span>$ {{ pr.price }}</span>
+        </mat-card-footer>
+        <mat-card-actions>
+          <a mat-button [routerLink]="['/products', pr.id]">View</a>
+        </mat-card-actions>
+      </mat-card>
     } @else {
       <product-skeleton></product-skeleton>
     }
