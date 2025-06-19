512d880e11d7e22d1cdc2925be34dd8226031290
Antonio Criado <140711500+AntonioCReyes@users.noreply.github.com>
2025-06-19 09:47:42 +0200

    Update routing config and clean standalone components (#5)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './product/product-list.component';
+import { ProductDetailComponent } from './product/product-detail.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/products', pathMatch: 'full' },
   { path: 'products', loadComponent: () => ProductListComponent },
+  { path: 'products/:id', loadComponent: () => ProductDetailComponent },
 ];
