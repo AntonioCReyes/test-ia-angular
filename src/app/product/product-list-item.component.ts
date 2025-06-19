a42243b6b23f3384ea4e253f2f018e1700df8ae2
Antonio Criado <140711500+AntonioCReyes@users.noreply.github.com>
2025-06-19 15:52:51 +0200

    feat(product): add list item component

---
+++ src/app/product/product-list-item.component.ts
@@ -0,0 +1,36 @@
+import { Component, ChangeDetectionStrategy, input } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { Product } from './product';
+
+@Component({
+  selector: 'product-list-item',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [RouterLink, NgOptimizedImage],
+  styles: [
+    `
+      :host {
+        display: flex;
+        align-items: center;
+        gap: 8px;
+        padding: 8px;
+        box-sizing: border-box;
+        height: 120px;
+      }
+    `,
+  ],
+  template: `
+    <a [routerLink]="['/products', product().id]" class="link">
+      <img
+        [ngSrc]="product().imageUrl"
+        alt="{{ product().name }}"
+        width="100"
+        height="100"
+      />
+      <span>{{ product().name }}</span>
+    </a>
+  `,
+})
+export class ProductListItemComponent {
+  product = input<Product>();
+}
