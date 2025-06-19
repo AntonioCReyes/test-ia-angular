74d2aa16cf2b56ca2ea45d654512cb99cdca8815
Antonio Criado <140711500+AntonioCReyes@users.noreply.github.com>
2025-06-19 21:14:23 +0200

    fix: improve virtual scroll grid support (#18)

---
+++ src/app/product/product-row-data-source.ts
@@ -0,0 +1,27 @@
+import { CollectionViewer, DataSource } from '@angular/cdk/collections';
+import { inject, Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Product } from './product';
+import { ProductDataSource } from './product-data-source';
+
+function chunkArray<T>(items: readonly T[], size: number): T[][] {
+  const chunks: T[][] = [];
+  for (let i = 0; i < items.length; i += size) {
+    chunks.push(items.slice(i, i + size));
+  }
+  return chunks;
+}
+
+@Injectable()
+export class ProductRowDataSource extends DataSource<(Product | undefined)[]> {
+  private base = inject(ProductDataSource);
+  private readonly perRow = 4;
+
+  connect(viewer: CollectionViewer): Observable<(Product | undefined)[][]> {
+    return this.base.connect(viewer).pipe(map((items) => chunkArray(items, this.perRow)));
+  }
+
+  disconnect(): void {
+    this.base.disconnect();
+  }
+}
